5d3988484c65008c684e732e919459b49e2b97a4
usagi. <141940017+usagi-dead@users.noreply.github.com>
2024-10-17 21:15:09 +0300

    fix filter input focus

--- client/src/ProductPage/Filter/FilterInput/FilterInput.jsx
+++ client/src/ProductPage/Filter/FilterInput/FilterInput.jsx
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./FilterInput.css";
 
 export default function FilterInput({ max, isClicked }) {
+    const [isActive, setIsActive] = useState(false); 
+
     function handleFilters(e) {
         e.stopPropagation();
     }
-    
+
+    function handleFocus() {
+        setIsActive(true); 
+    }
+
+    function handleBlur() {
+        setIsActive(false); 
+    }
+
     return (
-        <div className={`filters-input-container ${isClicked ? 'filters-input-show' : 'filters-hidden'}`} onClick={handleFilters} tabIndex="-1">
-            <input type="text" className="filter-input" placeholder='от' />
+        <div 
+            className={`filters-input-container ${isClicked ? 'filters-input-show' : 'filters-hidden'} ${isActive ? 'filters-input-active' : ''}`} 
+            onClick={handleFilters} 
+            tabIndex="-1"
+        >
+            <input 
+                type="text" 
+                className="filter-input" 
+                placeholder='от' 
+                onFocus={handleFocus} 
+                onBlur={handleBlur}
+            />
             <span className="filter-line" />
-            <input type="text" className="filter-input" placeholder={max} />
+            <input 
+                type="text" 
+                className="filter-input" 
+                placeholder={max} 
+                onFocus={handleFocus} 
+                onBlur={handleBlur}
+            />
         </div>
     );
 }
